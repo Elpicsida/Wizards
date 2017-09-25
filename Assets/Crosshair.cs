da527076884c6d0e4dd35fe22dffcd4896f456c6
Kava <elpicsida@gmail.com>
2017-09-25 19:10:46 +0200

    Branch#4 - Crosshair - Fixed bugs connected with crosshair - Crosshair is hidden when player is being deactivated

--- Assets/Crosshair.cs
+++ Assets/Crosshair.cs
@@ -7,25 +7,27 @@ public class Crosshair : MonoBehaviour {
     private GameObject parent;
     private WizzardController wizardController;
     private int radiusFromCenter = 2;
+    private SpriteRenderer spriteRenderer;
 
     // Use this for initialization
     void Start () {
         parent = transform.parent.gameObject;
         wizardController = parent.GetComponent<WizzardController>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (wizardController.InputEnabled)
         {
-            enabled = true;
+            spriteRenderer.enabled = true;
             var x = radiusFromCenter * Mathf.Cos(wizardController.Angle * Mathf.Deg2Rad) + parent.transform.position.x;
             var y = radiusFromCenter * Mathf.Sin(wizardController.Angle * Mathf.Deg2Rad) + parent.transform.position.y;
             transform.position = new Vector2(x, y);
         }
         else
         {
-            enabled = false;
+            spriteRenderer.enabled = false;
         }
 	}
 }
